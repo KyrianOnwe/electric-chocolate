f01f4fb864378542750d1ca82310bb16463a7857
Kyrian <dr.kyrian.onwe@gmail.com>
2021-09-26 20:18:41 -0500

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,6 @@
+const h2 = document.createElement("h2");
+const h3 = document.createElement("h3");
+h2.textContent = "This content added by JavaScript";
+h3.textContent = "Don't forget Kyrian";
+document.querySelector("body").appendChild(h2);
+document.querySelector("body").appendChild(h3);
